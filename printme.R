8efb2721f7d6800ea65a0be8166c057efeeb5367
Michael Graber <m.graber@campus.lmu.de>
2017-11-03 16:23:17 +0100

    imp printme

--- printme.R
+++ printme.R
@@ -1,4 +1,6 @@
 name = "<your name>"
 eyecolor = "<your eyevcolor>"
 meal = "<your favorite meal>"
+function(bla){}
 print(paste0(name, " has ", eyecolor, " eyes and enjoys eating ", meal, " the most!"))
+}
